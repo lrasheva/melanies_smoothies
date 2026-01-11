682c32869af4f453e7d6436e8c31602375225617
lrasheva <liliq.rasheva@gmail.com>
2026-01-11 16:34:12 +0200

    adding json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ if ingredients_list:
         st.dataframe(data=my_dataframe_result, use_container_width=True)
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
